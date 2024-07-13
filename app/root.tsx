652adef05c974cf0abcfa6a1a2ce838749bbfa08
Daewon Park <moochi@kakao.com>
2024-07-13 16:20:43 +0900

    add infinite scroll image

--- app/root.tsx
+++ app/root.tsx
@@ -19,6 +19,10 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
         <Meta />
         <Links />
       </head>
